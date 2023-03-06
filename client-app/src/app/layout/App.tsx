abe2619f3826040143919214389c271e78dab49b
Juan Garces <jpyval@gmail.com>
2023-03-06 18:38:00 -0500

    end section 6

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,20 +1,29 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import { Container} from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import {v4 as uuid} from 'uuid';
+import agent from '../api/agent';
+import LoadingComponent from './LoadingComponent';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [submitting, setSubmitting] = useState(false);
 
   useEffect( ()=>{
-    axios.get<Activity[]>('http://localhost:5000/api/activities')
-    .then(response =>{
-      setActivities(response.data);
+    agent.Activities.list().then(response =>{
+      let activities: Activity[] = [];
+
+      response.forEach((activity: Activity) =>{
+        activity.date = activity.date.split('T')[0];
+        activities.push(activity);
+      })
+      setActivities(activities);
+      setLoading(false);
     })
   }, [])
 
@@ -35,18 +44,40 @@ function App() {
   }
   
   function handleCreateOrEditActivity(activity: Activity){
+    setSubmitting(true);
     /* When ID editing is gonna filter the array of activities excluding the existing one and 
-    then add the one that has been passed to the method. This way we dont have to map the update of each field*/ 
-    activity.id
-    ? setActivities([...activities.filter(x=> x.id !== activity.id), activity])
-    : setActivities([...activities, {...activity, id: uuid()}]);
-    setEditMode(false);
-    setSelectedActivity(activity);
+    then add the one that has been passed to the method. This way we dont have to map the update of each field*/
+    
+    if (activity.id){
+      agent.Activities.update(activity).then(()=>{
+        setActivities([...activities.filter(x=> x.id !== activity.id), activity]);
+        setSelectedActivity(activity);
+        setEditMode(false);
+        setSubmitting(false);
+      })
+    } else {
+      activity.id = uuid();
+      agent.Activities.create(activity).then(()=>{
+        setActivities([...activities, activity]);
+        setSelectedActivity(activity);
+        setEditMode(false);
+        setSubmitting(false); 
+      })
+    }
+
   }
 
   function handleDeleteActivity(id: string){
-    setActivities([...activities.filter(x=> x.id !== id)])
+    setSubmitting(true);
+    agent.Activities.delete(id).then(()=>
+    {
+      setActivities([...activities.filter(x=> x.id !== id)]);
+      setSubmitting(false);
+    })
+    
   }
+
+  if (loading) return <LoadingComponent content='Loading app' />
 
   return (
     <>
@@ -62,6 +93,7 @@ function App() {
         closeForm = {handleFormClose} 
         createOrEdit = {handleCreateOrEditActivity}
         deleteActivity = {handleDeleteActivity}
+        submitting = {submitting}
         />
       </Container>
     </>
